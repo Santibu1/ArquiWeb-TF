90e576a5bb82e220f48ecc7263084dd448a97978
John <u20231a722@upc.edu.pe>
2025-10-05 12:59:30 -0500

    Creacion de login para los usuarios registrados

---
+++ src/main/java/com/upc/ecochipstf/dto/LoginDTO.java
@@ -0,0 +1,19 @@
+package com.upc.ecochipstf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String email;
+    private String password;
+    private String mensaje;
+    private Long usuarioId;
+    private String nombreCompleto;
+    private String rol;
+}
